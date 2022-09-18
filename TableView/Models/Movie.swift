15ea119f2bd6552c49d811759b5dafbac08e1c87
HoJun Choi <nyhj962@gmail.com>
2022-09-18 19:01:53 +0900

    TableView with Code

---
+++ TableView/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableView
+//
+//  Created by HoJun on 2022/09/18.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
